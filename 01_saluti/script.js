6eee01feed5eaf12e0b50e4bcf0a5b596b703081
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-22 17:50:19 +0100

    ADD first

---
+++ 01_saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function saluto(nome){
+    return `ciao ${nome}`
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(saluto(userName))
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
