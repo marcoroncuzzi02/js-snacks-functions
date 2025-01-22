1323af218efcc18dd8f167249076697b9134c92b
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-22 18:50:42 +0100

    ADD es4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,27 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const lettereParola = []
+const arrayVocali =["a", "e", "i", "o", "u"]
+const vocaliContenute =[]
 
 // Dichiara la funzione qui.
+function stringa(){
+    for (let i=0; i < word.length; i++){
+        lettereParola.push(word[i])
+        console.log (lettereParola)
+    }
+    for (let i=0; i < lettereParola.length; i++){
+        if (arrayVocali.includes(lettereParola[i])){
+            vocaliContenute.push(lettereParola[i])
+            console.log(vocaliContenute.length, vocaliContenute)
+        }
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+stringa()
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
